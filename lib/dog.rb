568352bd7df9067a671f76a1f645d509ef0bb45c
lizaetillman <lizaetillman@gmail.com>
2020-07-14 23:20:28 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+class Dog
+
+    @@all = []  #storage for each Dog instance created
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def self.all 
+        @@all 
+    end
+
+    def self.clear_all #.clear removes all elements from self => [ ]
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+
+    def save
+        @@all << self
+    end
+    
+    def name
+        @name
+    end
+
+end
